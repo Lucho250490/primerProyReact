efa76ac6a98f16832b9e1bfd308a65c687b53cc1
Lucho250490 <u20142129559@usco.edu.co>
2023-01-23 12:33:16 -0500

    Entregable 1 frases de superracio personal

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
 import QuoteButton from './QuoteButton'
 
-const QuoteBox = ({quote, newQuote, color}) => {
+const QuoteBox = ({quote, newQuote, color, imag}) => {
   return (
-    <div className='quoteBox' style={{color: color}}>
-      <p>{quote.quote}</p>
-      <h3 className='quoteBox_author'>{quote.author}</h3>
-      <i className='bx bxs-quote-left quoteBox_icon'></i>
+    <div className='quoteBox' style={{background:imag}} >
+
+      
+      
+      <article className="capa">
+
+      <figure>
+        <img src={imag.imag}/>
+      </figure>
+
+      
+
+      <p  style={{color:color}} >{quote.quote}</p>
+
+      <i className='bx bxs-quote-left quoteBox_icon'  style={{color:color}} ></i>
+
+      <h3 className='quoteBox_author' style={{color:color}}>{quote.author} </h3>
+
+
+      </article>
+      <h1 className="title" style={{color:color}}>Overcoming phrases
+      </h1>
+
+      
+      
+
       <QuoteButton newQuote = {newQuote} 
-      color = {color}/> 
+      color = {color}/>
+
+      
+      
+       
     </div>
   )
 }
